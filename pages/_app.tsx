8d17fe67b5fa84fda99a5d9d7be1e7a1d864ebe4
teitei-tk <teitei.tk@gmail.com>
2022-09-07 00:33:56 +0900

    render response users

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Suspense } from "react";
+import { CircularProgress } from "@mui/material";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Suspense fallback={<CircularProgress />}>
+      <Component {...pageProps} />;
+    </Suspense>
+  );
 }
 
-export default MyApp
+export default MyApp;
